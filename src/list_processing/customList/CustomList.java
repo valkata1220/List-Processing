a709e957343570f225ac8e740f55942284b95549
Stefan <s.d.neshkov@gamil.com>
2017-11-15 20:23:09 +0200

    Implemented prepend command

--- src/list_processing/customList/CustomList.java
+++ src/list_processing/customList/CustomList.java
@@ -1,6 +1,7 @@
 package list_processing.customList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class CustomList {
@@ -10,5 +11,22 @@ public class CustomList {
         this.customList = new ArrayList<>();
     }
 
+    public String prepend(String inputToken) {
+        int len = this.customList.size() + 1;
+        String[] temp = new String[len];
 
+        temp[0] = inputToken;
+
+        for (int i = 1; i < len - 1; i++) {
+
+            temp[1] = this.customList.get(1);
+        }
+
+        List<String> result = new ArrayList<>();
+        result.addAll(Arrays.asList(temp));
+
+        return String.join(" ", result);
+
+        /* By Stefan - Java trainee */
+    }
 }

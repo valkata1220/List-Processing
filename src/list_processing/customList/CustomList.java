63f20cd0d4a835adfcf61da96d0137d148f5834c
Valentin <valkata1220@gmail.com>
2017-11-15 20:16:45 +0200

    Reverse functionality completed!

--- src/list_processing/customList/CustomList.java
+++ src/list_processing/customList/CustomList.java
@@ -9,6 +9,17 @@ public class CustomList {
     public CustomList() {
         this.customList = new ArrayList<>();
     }
+    public void inisializeCustomList(List<String> tokens){
+        this.customList.addAll(tokens);
+    }
+    
+    public String reverse(){
+        List<String> reversedList = new ArrayList<>();
+        for (int i = this.customList.size()-1; i >= 0; i--) {
+             reversedList.add(this.customList.get(i));
+        }
 
-
+        this.customList = reversedList;
+        return String.join(" ",this.customList);
+    }
 }
--- src/list_processing/engine/Engine.java
+++ src/list_processing/engine/Engine.java
@@ -4,6 +4,10 @@ import list_processing.customList.CustomList;
 import list_processing.io.ConsoleInputReader;
 import list_processing.io.ConsoleOutputReader;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Engine {
     private ConsoleInputReader inputReader;
     private ConsoleOutputReader outputReader;
@@ -16,7 +20,21 @@ public class Engine {
     }
 
     public void run() {
-        String line = "";
+        String line = this.inputReader.readLine();
+        List<String> firstList = Arrays.stream(line.split("\\s+")).collect(Collectors.toList());
+        this.customList.inisializeCustomList(firstList);
+
+        while (true) {
+            line = this.inputReader.readLine();
+            String[] tokkens = line.split("\\s+");
+            executeCommand(tokkens);
+            if ("end".equals(line)) {
+                break;
+            }
+        }
+    }
+
+    private void executeCommand(String[] tokkens) {
 
     }
 }

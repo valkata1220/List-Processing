f4499cb9d78e9774a33817c0af4de777a2c7afa1
Valentin <valkata1220@gmail.com>
2017-11-15 19:04:30 +0200

    Implemented Engine 2

---
+++ src/list_processing/engine/Engine.java
@@ -0,0 +1,18 @@
+package list_processing.engine;
+
+import list_processing.io.ConsoleInputReader;
+import list_processing.io.ConsoleOutputReader;
+
+public class Engine {
+    private ConsoleInputReader inputReader;
+    private ConsoleOutputReader outputReader;
+
+    public Engine(ConsoleInputReader inputReader, ConsoleOutputReader outputReader) {
+        this.inputReader = inputReader;
+        this.outputReader = outputReader;
+    }
+
+    public void run(){
+
+    }
+}
---
+++ src/list_processing/io/ConsoleOutputReader.java
@@ -0,0 +1,8 @@
+package list_processing.io;
+
+public class ConsoleOutputReader {
+
+    public void write(String output){
+        System.out.println(output);
+    }
+}

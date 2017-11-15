d722d0f7a7a81c03c18e4d4c4701ad083430fbb1
Stefan <s.d.neshkov@gamil.com>
2017-11-15 21:12:17 +0200

    implemented rollLeft

--- src/list_processing/customList/CustomList.java
+++ src/list_processing/customList/CustomList.java
@@ -10,70 +10,69 @@ public class CustomList {
     public CustomList() {
         this.customList = new ArrayList<>();
     }
-  
+
 
     public void inisializeCustomList(List<String> tokens) {
         this.customList.addAll(tokens);
     }
-    
+
 
     public String reverse() {
         List<String> reversedList = new ArrayList<>();
-      
+
         for (int i = this.customList.size() - 1; i >= 0; i--) {
             reversedList.add(this.customList.get(i));
         }
         this.customList = reversedList;
 
-        return String.join(" ",this.customList);
+        return String.join(" ", this.customList);
 
-}
+    }
 
-        public String prepend (String inputToken){
-            int len = this.customList.size() + 1;
-            String[] temp = new String[len];
+    public String prepend(String inputToken) {
+        int len = this.customList.size() + 1;
+        String[] temp = new String[len];
 
-            temp[0] = inputToken;
+        temp[0] = inputToken;
 
-            for (int i = 1; i < len - 1; i++) {
+        for (int i = 1; i < len - 1; i++) {
 
-                temp[1] = this.customList.get(1);
-            }
+            temp[1] = this.customList.get(1);
+        }
 
-            List<String> result = new ArrayList<>();
-            result.addAll(Arrays.asList(temp));
+        List<String> result = new ArrayList<>();
+        result.addAll(Arrays.asList(temp));
 
-            this.customList = result;
+        this.customList = result;
 
         /* By Stefan - Java trainee */
 
-            return String.join(" ", this.customList);
+        return String.join(" ", this.customList);
 
-        }
- 
-public String append(String token){
+    }
+
+    public String append(String token) {
         List<String> appendList = new ArrayList<>();
 
-        for(int i=0; i<this.customList.size(); i++){
+        for (int i = 0; i < this.customList.size(); i++) {
             appendList.add(this.customList.get(i));
         }
         appendList.add(token);
         this.customList = appendList;
-        return String.join(" ",this.customList);
+        return String.join(" ", this.customList);
 
-        
 
     }
 
-    public String insert(int index,String string){
-        if(index < 0 || index > this.customList.size()-1){
-            return String.format("Error: invalid index %d",index);
+    public String insert(int index, String string) {
+        if (index < 0 || index > this.customList.size() - 1) {
+            return String.format("Error: invalid index %d", index);
         }
         List<String> tempList = new ArrayList<>();
 
-        for (int i = 0; i < this.customList.size()-1; i++) {
-            if(index == i){
-                if(i == this.customList.size()-1){
+        for (int i = 0; i < this.customList.size() - 1; i++) {
+            if (index == i) {
+                if (i == this.customList.size() - 1) {
                     tempList.add(this.customList.get(i));
                     tempList.add(string);
                     break;
@@ -84,6 +83,79 @@ public String append(String token){
         }
 
         this.customList = tempList;
-        return String.join(" ",this.customList);
+        return String.join(" ", this.customList);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String rollLeft(){
+        List<String> temp = new ArrayList<>();
+        for (int i = 1; i < this.customList.size(); i++) {
+
+            temp.add(this.customList.get(i));
+        }
+
+        temp.add(this.customList.get(0));
+        this.customList = temp;
+
+
+        return String.join(" ", this.customList);
     }
 }

59c2c4e7a9758161fc0ee04dc930cf793575010b
Valentin <valkata1220@gmail.com>
2017-11-15 18:52:44 +0200

    Implemented Engine

--- src/list_processing/Main.java
+++ src/list_processing/Main.java
@@ -1,4 +1,15 @@
 package list_processing;
 
+import list_processing.engine.Engine;
+import list_processing.io.ConsoleInputReader;
+import list_processing.io.ConsoleOutputReader;
+
 public class Main {
+    public static void main(String[] args) {
+        ConsoleInputReader inputReader = new ConsoleInputReader();
+        ConsoleOutputReader outputReader = new ConsoleOutputReader();
+
+        Engine engine = new Engine(inputReader, outputReader);
+        engine.run();
+    }
 }
